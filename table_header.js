f56ff06fbe3bb80d88d565b7f580886a2305851d
onedean <onedean@foxmail.com>
2023-09-01 23:33:18 +0800

    newbranch

---
+++ table_header.js
@@ -0,0 +1,29 @@
+document.write("<table width=\"850\" border=\"0\" height=\"184\">");
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<td> <img src=\"Images\/wei_chen.jpg\" alt=\"Wei Chen\"");
+document.write("width=\"150\" height=\"200\"> <\/td>");
+document.write("<td>");
+document.write("<h1>Wei Chen</h1>");
+document.write("<br>");
+document.write("<b>Ph.D. Student</b>");
+document.write("<br>");
+
+document.write("<a href=\"https:\/\/www.hkust-gz.edu.cn\">Hong Kong University of Science and Technology (Guangzhou)<\/a></b>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<b>Contact info:</b><br>");
+document.write(" Nansha, Guangzhou, China. <br>");
+document.write("E-mail: <a href=\"mailto:onedean@gmail.com\" title=\"\"> onedean@foxmail.com </a> <br>");
+document.write("<br>");
+document.write("&nbsp;");
+document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=RCfQIcQAAAAJ&hl\"><img src=\"Images\/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> Google Scholar <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/github.com\/onedean\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> Github <\/a> &nbsp;");
+document.write("<a href=\"doc\/CV-WeiChen.pdf\"><img src=\"Images\/cv.png\" alt=\"CV\" width=\"25\" border=\"0\" height=\"25\"> CV <\/a> &nbsp;");
+// document.write("<a href=\"https:\/\/www.zhihu.com\/people\/its312\/posts\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
+// document.write("<a href=\"https:\/\/twitter.com\/chenxy346\/\"> <img src=\"Images\/twitter.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
+document.write("<\/td>");
+document.write("<\/tr>");
+document.write("<\/tbody>");
+document.write("<\/table>");
